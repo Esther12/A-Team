96bbcfcaff69fa44248f8443b115a6a07c8ce2b7
Yating(Esther) Su <yaringsu@foxmail.com>
2019-07-22 11:55:57 -0400

    add js file to load sheleeza's file

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,1 +1,46 @@
-javascript will go here
+$("#find-movie").on("click", function(event) {
+
+     
+    event.preventDefault();
+    
+
+    var movie = $("#search").val();
+
+
+    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+          
+      
+
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      $("#title").html("Title: " + response.Title);
+    //  $("").html("Rating: " + response.Ratings[1].Value + " Rotten Tomatoes");
+      $("#year").html("Released: " + response.Released);
+      $("#directors").html("Plot: " +response.Plot);
+      $("#poster").attr("src",response.Poster);
+      $("#actors").html("Actors: " + response.Actors);
+      $("#duration").html("Duration: " + response.Runtime);
+      $("#genre").html("Genre: "  + response.Genre)
+      $("#rating").html("Rating: " + response.imdbRating)
+      $(".writer").html("Writer: "  + response.Writer)
+
+      console.log(response);
+
+      $("#movie-input").val("");
+
+  
+    })
+
+    
+    
+    /*$.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      $("#movie-view").text(JSON.stringify(response));
+    });*/
+
+  });
