ba08fe2b83127953b6d281287d1546aee4fb79b1
Yating(Esther) Su <yaringsu@foxmail.com>
2019-11-12 21:55:47 -0500

    Cannot delete the keys

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {
+  YOUTUBE_KEY: "AIzaSyA6JVzEaTSEgPcgCKyIaIwQW8S - RuEbw3s",
+  GIFFY_KEY: "6kzr50l8dlgEaOOVqe1VMiOwUmuGt3p6",
+  FIRE_BASE_KEY: "AIzaSyB4o9RSM4MuAdY7LklNwhWoeux1mYFbQ0k",
+  MOVIE_KEY: "&apikey=trilogy"
+};
